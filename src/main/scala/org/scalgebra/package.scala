510b492bb9a8dc26464b2849f0dd357545960b6a
itegulov <ditegulov@gmail.com>
2016-04-29 22:57:51 +0300

    Document main scalgebra package object

--- src/main/scala/org/scalgebra/package.scala
+++ src/main/scala/org/scalgebra/package.scala
@@ -1,6 +1,8 @@
 package org
 
 /**
+  * Accumulates all implicit operators for Vector and Matrix.
+  *
   * @author Daniyar Itegulov
   */
 package object scalgebra extends DenseMatrixOps with DenseVectorOps
