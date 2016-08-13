ad0914177aa03b6421fa0df483017558582f969b
Daniyar Itegulov <ditegulov@gmail.com>
2016-08-13 18:41:13 +0300

    Fix Vector's float and double specification
    Forgot to add asserts, also need tolerance constructor

--- src/test/scala/org/scalgebra/DenseVectorDoubleSpec.scala
+++ src/test/scala/org/scalgebra/DenseVectorDoubleSpec.scala
@@ -9,15 +9,26 @@ import scala.math.sqrt
   * @author Daniyar Itegulov
   */
 class DenseVectorDoubleSpec extends FlatSpec with Matchers {
+  def tolerantDenseVectorDoubleEquality(implicit equalityDouble: Equality[Double]): Equality[DenseVector[Double]] =
+    new Equality[DenseVector[Double]] {
+      override def areEqual(a: DenseVector[Double], b: Any): Boolean = b match {
+        case bVec: DenseVector[_] if a.length == bVec.length =>
+          (0 until a.length).forall(i => equalityDouble.areEqual(a(i), bVec(i)))
+        case _ =>
+          false
+      }
+    }
+
   implicit val doubleEq = TolerantNumerics.tolerantDoubleEquality(1e-6)
+  implicit val denseVectorDoubleEq = tolerantDenseVectorDoubleEquality
 
   "DenseVector[Double]" should "be able to normalize" in {
     val vector: DenseVector[Double] = DenseVector(1D, 1D)
-    vector.normalize === DenseVector(sqrt(2.0D) / 2.0D, sqrt(2.0D) / 2.0D)
+    assert(vector.normalize === DenseVector(sqrt(2.0D) / 2.0D, sqrt(2.0D) / 2.0D))
   }
 
   "DenseVector[Double]" should "be able to calculate magnitude" in {
     val vector: DenseVector[Double] = DenseVector(1D, 1D)
-    vector.magnitude === sqrt(2.0D)
+    assert(vector.magnitude === sqrt(2.0D))
   }
 }
--- src/test/scala/org/scalgebra/DenseVectorFloatSpec.scala
+++ src/test/scala/org/scalgebra/DenseVectorFloatSpec.scala
@@ -9,15 +9,26 @@ import scala.math.sqrt
   * @author Daniyar Itegulov
   */
 class DenseVectorFloatSpec extends FlatSpec with Matchers {
-  implicit val floatEq = TolerantNumerics.tolerantFloatEquality(1e-3f)
+  def tolerantDenseVectorFloatEquality(implicit equalityFloat: Equality[Float]): Equality[DenseVector[Float]] =
+    new Equality[DenseVector[Float]] {
+      override def areEqual(a: DenseVector[Float], b: Any): Boolean = b match {
+        case bVec: DenseVector[_] if a.length == bVec.length =>
+          (0 until a.length).forall(i => equalityFloat.areEqual(a(i), bVec(i)))
+        case _ =>
+          false
+      }
+    }
+
+  implicit val floatEq = TolerantNumerics.tolerantFloatEquality(1e-3F)
+  implicit val denseVectorFloatEq = tolerantDenseVectorFloatEquality
 
   "DenseVector[Float]" should "be able to normalize" in {
     val vector: DenseVector[Float] = DenseVector(1F, 1F)
-    vector.normalize === DenseVector(sqrt(2.0F) / 2.0F, sqrt(2.0F) / 2.0F)
+    assert(vector.normalize === DenseVector(sqrt(2.0F).toFloat / 2.0F, sqrt(2.0F).toFloat / 2.0F))
   }
 
   "DenseVector[Float]" should "be able to calculate magnitude" in {
     val vector: DenseVector[Float] = DenseVector(1F, 1F)
-    vector.magnitude === sqrt(2.0F)
+    assert(vector.magnitude === sqrt(2.0F).toFloat)
   }
 }
